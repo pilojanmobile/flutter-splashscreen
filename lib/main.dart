671710ad2e01d3c33b19bf3c9fdefdf15d68f500
Pilojanmobile <pilojan992@gmail.com>
2021-01-04 21:28:54 +0530

    splash screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'myhomepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Splash Screen',
+      theme: ThemeData(
+        
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
---
+++ lib/myhomepage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'menu.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    //set time to load the new page
+    Future.delayed(Duration(seconds: 60), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Menu()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: 300,
+                width: 300,
+                child: Lottie.asset('assets/splashback.json')),
+            SizedBox(height: 20),
+            Text(
+              "Dodo",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
